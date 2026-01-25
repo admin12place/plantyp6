983b10c7140617175703034a86c1de5d5383f7ae
admin12place <thierry@randovelo34.fr>
2026-01-25 14:05:34 +0100

    page commander fonctionnelle

--- form-command.php
+++ form-command.php
@@ -8,35 +8,42 @@ function command_form_p6(){
     ob_start();
 ?>
 
-<form method="post" action="" class="command-form">
+<form method="post" class="command-form" >
+ <?php wp_nonce_field( 'form_command_action', 'form_command_nonce' ); ?>
+
     <main>
+    
         <div id="bloc-images">
-            <div class="image-count">
-                <div class="background-title bt-1">
-                    <h4 class="full-center-title">FRAISE</h4>
+            <div class="bloc-2images bloc-2images-left">
+                <div class="image-count">
+                    <div class="background-title bt-1">
+                        <h4 class="full-center-title">FRAISE</h4>
+                    </div>
+                    <input type="number" id="count-fraise" class="counter-cmd" name="fraise" value="0" min="0" max="12"/>
                 </div>
-                <input type="number" id="count-fraise" class="counter-cmd" name="fraise" value="0" min="0" max="12"/>
-            </div>
 
-            <div class="image-count">
-                <div class="background-title bt-2">
-                    <h4 class="full-center-title">PAMPLE</br>MOUSSE</h4>
+                <div class="image-count">
+                    <div class="background-title bt-2">
+                        <h4 class="full-center-title">PAMPLE</br>MOUSSE</h4>
+                    </div>
+                    <input type="number" id="count-pamplemousse" class="counter-cmd" name="pamplemousse" value="0" min="0" max="12"/>
                 </div>
-                <input type="number" id="count-pamplemousse" class="counter-cmd" name="pamplemousse" value="0" min="0" max="12"/>
             </div>
-
-            <div class="image-count">
-                <div class="background-title bt-3">
-                    <h4 class="full-center-title">FRAM</br>BOISE</h4>
+            
+            <div class="bloc-2images bloc-2images-right">
+                <div class="image-count">
+                    <div class="background-title bt-3">
+                        <h4 class="full-center-title">FRAM</br>BOISE</h4>
+                    </div>
+                    <input type="number" id="count-framboise" class="counter-cmd" name="framboise" value="0" min="0" max="12"/>
                 </div>
-                <input type="number" id="count-framboise" class="counter-cmd" name="framboise" value="0" min="0" max="12"/>
-            </div>
 
-            <div class="image-count">
-                <div class="background-title bt-4">
-                    <h4 class="full-center-title">CITRON</h4>
+                <div class="image-count">
+                    <div class="background-title bt-4">
+                        <h4 class="full-center-title">CITRON</h4>
+                    </div>
+                    <input type="number" id="count-citron" class="counter-cmd" name="citron" value="0" min="0" max="12"/>
                 </div>
-                <input type="number" id="count-citron" class="counter-cmd" name="citron" value="0" min="0" max="12"/>
             </div>
         </div>
 
@@ -44,53 +51,60 @@ function command_form_p6(){
 
         <div id="bloc-form">
         
-            <div class="bloc-form-right">
+            <div class="bloc-form-left">
             
                 <h3 class="bloc-form-title bloc-informations">Vos informations</h3>
 
                 <div class="bloc-form-champs">
-                    <label for="username">Nom</label>
-                    <input  type="text" id="username" name="username" required size="40"/>
+                    <label for="username" class="label-cmd">Nom</label>
+                    <input  type="text" id="username" name="username" class="input-cmd" required size="40"/>
                 </div>
 
                 <div class="bloc-form-champs">
-                    <label for="userlastname">Prénom</label>
-                    <input  type="text" id="userlastname" name="userlastname" required size="40"/>
+                    <label for="userlastname" class="label-cmd">Prénom</label>
+                    <input  type="text" id="userlastname" name="userlastname" class="input-cmd" required size="40"/>
                 </div>
 
                 <div class="bloc-form-champs">
-                    <label for="usermail">Mail</label>
-                    <input  type="email" id="usermail" name="usermail" required size="40"/>
+                    <label for="usermail" class="label-cmd">Mail</label>
+                    <input  type="email" id="usermail" name="usermail" class="input-cmd" required size="40"/>
                 </div>
 
             </div>
 
             <div class="form-vertical-separator"></div>
 
-            <div class="bloc-form-left">
+            <div class="bloc-form-right">
             
                 <h3 class="bloc-form-title bloc-livraison">Livraison</h3>
 
                 <div class="bloc-form-champs">
-                    <label for="useradresse">Adresse</label>
-                    <input  type="text" id="useradresse" name="useradresse" required size="40"/>
+                    <label for="useradresse" class="label-cmd">Adresse</label>
+                    <input  type="text" id="useradresse" name="useradresse" class="input-cmd" required size="40"/>
                 </div>
 
                 <div class="bloc-form-champs">
-                    <label for="userpostcode">Code postal</label>
-                    <input  type="text" id="userpostcode" name="userpostcode" required size="40"/>
+                    <label for="userpostcode" class="label-cmd">Code postal</label>
+                    <input  type="text" id="userpostcode" name="userpostcode" class="input-cmd" required size="40"/>
                 </div>
 
                 <div class="bloc-form-champs">
-                    <label for="usertown">Ville</label>
-                    <input  type="text" id="usertown" name="usertown" required size="40"/>
+                    <label for="usertown" class="label-cmd">Ville</label>
+                    <input  type="text" id="usertown" name="usertown" class="input-cmd" required size="30"/>
                 </div>
             
             </div>
             
         </div>
 
-        <input type="submit" value="Commander"/>
+        <div class="cmd-form-submit">
+            <input type="submit" name="command-submit" class="submit-button-cmd" value="Commander"/>
+        </div>
+
+        <?php if ( isset($_POST['command-submit']) ) : ?>
+            <p>Commande envoyée avec succès.</p>
+        <?php endif; ?>
+
     </main>
 
 </form>
--- functions.php
+++ functions.php
@@ -11,3 +11,49 @@ require_once get_stylesheet_directory() . '/form-command.php';
 
 add_shortcode('formulaire_p6_commande', 'command_form_p6');
 
+function send_command_form()
+    {
+        error_log('send_command_form exécutée');
+        
+        if (!isset($_POST['command-submit']))
+            { return;}
+            
+        if (!isset($_POST['form_command_nonce']) || !wp_verify_nonce($_POST['form_command_nonce'], 'form_command_action'))
+            { return;}
+            
+        //Récupèration des données du formulaire et sécurisation
+        $lastname = sanitize_text_field( $_POST['username'] ?? '' );
+        $firstname = sanitize_text_field( $_POST['userlastname'] ?? '' );
+        $fullName = $lastname.' '.$firstname;
+        $mail = sanitize_email( $_POST['usermail'] ?? '' );
+        $adresse = sanitize_text_field( $_POST['useradresse'] ?? '' );
+        $postcode = sanitize_text_field( $_POST['userpostcode'] ?? '' );
+        $town = sanitize_text_field( $_POST['usertown'] ?? '' );
+
+        $date = date ('d/m/Y');
+
+        $fraise = sanitize_text_field( $_POST['fraise'] ?? '' );
+        $pamplemousse = sanitize_text_field( $_POST['pamplemousse'] ?? '' );
+        $citron = sanitize_text_field( $_POST['citron'] ?? '' );
+        $orange = sanitize_text_field( $_POST['orange'] ?? '' );
+
+        //définition de l'administrateur du site comme destinataire du mail
+        $dest = get_option( 'admin_email' );
+
+        //définition du sujet du mail
+        $subject = 'nouvelle commande de : '. $fullName;
+
+        //création du corps du message
+        $mail_body = 'DATE DE COMMANDE : ' . $date . '\n';
+        $mail_body .= 'NOM DU CLIENT : ' . $fullName . '\n';
+        $mail_body .= '.........';
+
+        //création des headers
+        $headers = ['Content-Type: text/plain; charset=UTF-8', 'Reply-To: ' . $fullName . ' <' . $mail . '>' ,];
+
+        //envoi du mail
+        wp_mail( $dest, $subject, $mail_body, $headers );
+    }
+
+add_action( 'init', 'send_command_form');
+
