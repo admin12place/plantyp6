4d7ee5dc34d4d2b0608ccd45c0cf4fb41e2c2327
admin12place <thierry@randovelo34.fr>
2026-01-30 08:30:53 +0100

    init

---
+++ html/wp-content/languages/plugins/wp-rollback-fr_FR.l10n.php
@@ -0,0 +1,2 @@
+<?php
+return ['x-generator'=>'GlotPress/4.0.3','translation-revision-date'=>'2024-01-01 17:53:38+0000','plural-forms'=>'nplurals=2; plural=n > 1;','project-id-version'=>'Plugins - WP Rollback &#8211; Rollback Plugins and Themes - Stable (latest release)','language'=>'fr','messages'=>['https://wprollback.com/'=>'https://wprollback.com/','WP Rollback'=>'WP Rollback ','Rollback (or forward) any WordPress.org plugin, theme or block like a boss.'=>'Retour vers les versions précédentes de toute extension ou thème WordPress.org comme un chef.']];
---
+++ html/wp-content/themes/plantyp6-main.old/plantyp6-main/footer.php
@@ -0,0 +1,13 @@
+            </main>
+            <?php //get_sidebar(); ?>
+        </div><!--Fin de la div "container"-->
+
+        <footer id="footer" role="contentinfo">
+            <div id="policy">
+                <a href="#">Mentions légales</a>
+            </div>
+        </footer>
+    </div>
+        <?php wp_footer(); ?>
+</body>
+</html>
